a9f69af7baf932c339f343b7c69d5c3673cb9c81
Thiago Alves dos Santos <tasantos@mppe.mp.br>
2025-06-08 17:00:18 -0300

    removendo tag da mensagem de log

--- thiagoads.py
+++ thiagoads.py
@@ -14,11 +14,11 @@ def get_subset(dataset, subset_param, seed=None):
     The optional seed parameter is used to set the random generator seed.
     """
     if subset_param is None:
-        logger.info("thiagoads: subset_param é None. Retornando o dataset original.")
+        logger.info("subset_param é None. Retornando o dataset original.")
         return dataset
 
     if seed is not None:
-        logger.info(f"thiagoads: Seed {seed} foi fornecida. Definindo a semente do gerador aleatório.")
+        logger.info(f"Seed {seed} foi fornecida. Definindo a semente do gerador aleatório.")
         torch.manual_seed(seed)
 
     dataset_length = len(dataset)
@@ -26,15 +26,15 @@ def get_subset(dataset, subset_param, seed=None):
     if isinstance(subset_param, float):
         if not (0 < subset_param <= 1):
             raise ValueError("thiagoads: Percentage must be between 0 and 1.")
-        logger.info(f"thiagoads: Método get_subset foi invocado para criar um subset com {subset_param * 100:.2f}% do dataset.")
+        logger.info(f"Método get_subset foi invocado para criar um subset com {subset_param * 100:.2f}% do dataset.")
         idxs = torch.randperm(dataset_length)[:int(dataset_length * subset_param)]
     elif isinstance(subset_param, int):
         if subset_param > dataset_length:
-            raise ValueError("thiagoads: num_samples cannot be greater than the dataset length.")
-        logger.info(f"thiagoads: Método get_subset foi invocado para criar um subset com {subset_param} exemplos.")
+            raise ValueError("num_samples cannot be greater than the dataset length.")
+        logger.info(f"Método get_subset foi invocado para criar um subset com {subset_param} exemplos.")
         idxs = torch.randperm(dataset_length)[:subset_param]
     else:
         raise TypeError("thiagoads: subset_param must be a float (percentage) or an int (number of samples).")
 
-    logger.info(f"thiagoads: Dataset original: {dataset_length} exemplos | Subset final: {len(idxs)} exemplos.")
+    logger.info(f"Dataset original: {dataset_length} exemplos | Subset final: {len(idxs)} exemplos.")
     return torch.utils.data.Subset(dataset, idxs)
